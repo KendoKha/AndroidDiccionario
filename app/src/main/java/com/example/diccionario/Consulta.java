73c44386e02b475622d4a0e597a2959d822207f2
KendoKha <Davidkendo7@gmail.com>
2021-05-11 11:13:39 +0200

    Hemos metido los radio buttons y el orden y ole con ole y olé

--- app/src/main/java/com/example/diccionario/Consulta.java
+++ app/src/main/java/com/example/diccionario/Consulta.java
@@ -10,7 +10,9 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioButton;
 
 public class Consulta extends AppCompatActivity {
 
@@ -21,10 +23,22 @@ public class Consulta extends AppCompatActivity {
 
 
         ArrayAdapter<Palabra> adaptador = new ArrayAdapter<Palabra>
-                (this, android.R.layout.simple_list_item_1, Diccionario.getDiccionario());
+                (this, android.R.layout.simple_list_item_1, Diccionario.consulta());
 
         ListView lista = (ListView) findViewById(R.id.lista);
         lista.setAdapter(adaptador);
+
+
+        //TODO EL TEMA DEL ORDEN
+
+        Button btnOrdenar = (Button) findViewById(R.id.btnOrdenar);
+        RadioButton rdBtnAlfabetico = (RadioButton) findViewById(R.id.rdBtnAlfabetico);
+        RadioButton rdBtnOrdenPalabras = (RadioButton) findViewById(R.id.rdBtnOrdenPalabra);
+        RadioButton rdBtnOrdenExpresion = (RadioButton) findViewById(R.id.rdBtnOrdenExpresion);
+
+
+
+
 
     }
 }
--- app/src/main/java/com/example/diccionario/Diccionario.java
+++ app/src/main/java/com/example/diccionario/Diccionario.java
@@ -16,5 +16,15 @@ public class Diccionario {
         return palabras;
     }
 
+    public static ArrayList consulta(){
+        ArrayList consulta = new ArrayList();
+
+        for(int i =0; i<palabras.size();i++){
+            consulta.add("Palabra castellano: "+palabras.get(i).dimePalabraEsp()+"     Palabra inglés"+palabras.get(i).dimePalabraIng());
+        }
+
+        return consulta;
+    }
+
 
 }
